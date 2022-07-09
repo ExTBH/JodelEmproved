ee5c48c4fe0beee91cc24c1e255e99c0eb1b5346
Natheer <nadeerzaher@outlook.com>
2022-07-09 19:33:08 +0300

    Boring Fixes

--- Theos/JDEViewController.h
+++ Theos/JDEViewController.h
@@ -2,8 +2,7 @@
 #import "Classes/JDEButtons.h"
 #import "Classes/JDESettingsManager.h"
 #import "Classes/JDEMapView.h"
-#define PATH "var/mobile/Library/Application Support/JodelEmproved.bundle"
-#define VERSION "Jodel EMPROVED By @ExTBH (1)"
+#define VERSION "Jodel EMPROVED By @ExTBH (1.0.1)"
 
 @interface JDEViewController : UIViewController
 - (void)viewDidLoad;
